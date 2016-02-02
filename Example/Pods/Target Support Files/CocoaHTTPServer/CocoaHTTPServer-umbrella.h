31dab800e80b91f9a117b7ca4aa3b5efceb2c92e
pplant <plant.peter.bz@gmail.com>
2016-02-02 12:36:19 +0100

    Ported to cocoapod lib

---
+++ Example/Pods/Target Support Files/CocoaHTTPServer/CocoaHTTPServer-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-CocoaHTTPServer_Tests/Pods-CocoaHTTPServer_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CocoaHTTPServer_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CocoaHTTPServer_TestsVersionString[];
+
